4f44302ce63fc9a08abc2e068481867013710f36
Sahil Sharma <sahilsharma2000dtc@gmail.com>
2024-02-13 21:29:58 +0530

    making project work

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useDispatch, useSelector } from 'react-redux'
+import {decrement, increment} from './reduxStore/features/counter/counterSlice'
 import './App.css'
 
 function App() {
   
+  const count = useSelector((state) => state.counter);
+  
+  const dispatch = useDispatch();
 
   return (
     <>
      <h1>Redux in React </h1>
+     <h2>Count: {count}</h2>
+     <button onClick={() => dispatch(increment())}>Increment</button>
+     <button onClick={() => dispatch(decrement())}>Decrement</button>
     </>
   )
 }
